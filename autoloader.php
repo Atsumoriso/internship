69e9240c54e216d68b6f0fdb99357f40249cd3bb
Ruslan Dovgospiny <ruslandovg291@gmail.com>
2016-06-23 19:02:47 +0300

    1)Change __autoload on spl_autoloader 2) Cut it in separate file autoloader.php

---
+++ autoloader.php
@@ -0,0 +1,21 @@
+<?php
+
+spl_autoload_register(function($classname) {
+    $sitePath = $_SERVER['DOCUMENT_ROOT'];
+    $filename = explode("\\", $classname);
+
+    for($i=0; $i<count($filename)-1; $i++)
+    {
+        $filename[$i] = lcfirst($filename[$i]);
+    }
+
+    $filename = implode('/', $filename);
+    $file = $sitePath . '/' . $filename . '.php';
+
+    if (!file_exists($file))
+    {
+        return FALSE;
+    }
+
+    require_once $file;
+});
--- index.php
+++ index.php
@@ -1,27 +1,9 @@
 <?php
-
-function __autoload($classname) {
-    $sitePath = $_SERVER['DOCUMENT_ROOT'];
-    $filename = explode("\\", $classname);
-
-    for($i=0; $i<count($filename)-1; $i++)
-    {
-       $filename[$i] = lcfirst($filename[$i]);
-    }
-
-    $filename = implode('/', $filename);
-    $file = $sitePath . '/' . $filename . '.php';
-
-    if (!file_exists($file))
-    {
-        return FALSE;
-    }
-
-    require_once $file;
-}
+require('autoloader.php');
 
 $dbLoger = new Core\Classes\LoggerDB;
 $dbLoger->error('my custom error msg, it work2');
 
 $fileLoger = new Core\Classes\LoggerFile;
 $fileLoger->error('my custom error msg');
+
